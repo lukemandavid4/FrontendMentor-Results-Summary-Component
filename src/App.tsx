aa19754994077192bfb5f6136b59086196c835dd
lukemandavid4 <lukemandavid4@gmail.com>
2024-06-25 23:24:28 +0300

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -20,49 +20,59 @@ const App = () => {
             You scored higher than 65% of the people who have taken these tests.
           </p>
         </div>
-        <div className="w-[50%] flex flex-col items-center gap-[0.5rem] p-[1rem]">
-          <h6 className="text-grayBlue font-bold">Summary</h6>
-          <div className="flex justify-between w-[100%]  bg-[hsla(0,_100%,_67%,_0.1)]">
-            <div className="flex">
-              <img src={Reaction} alt="Reaction" />
-              <span className="text-lightRed font-bold">Reaction</span>
+        <div className="w-[50%] flex flex-col items-center gap-[0.5rem] p-[1rem] shadow-grayBlue shadow-md">
+          <h6 className="text-grayBlue font-bold self-start">Summary</h6>
+          <div className="flex justify-between w-[100%]  bg-[hsla(0,_100%,_67%,_0.1)] p-[0.5rem] items-center rounded-lg">
+            <div className="flex gap-[0.3rem]">
+              <img src={Reaction} alt="Reaction" className="w-[1.1rem]" />
+              <span className="text-lightRed font-bold text-[0.9rem]">
+                Reaction
+              </span>
             </div>
-            <div>
+            <div className="text-[0.9rem]">
               <span className="text-grayBlue font-bold">80</span>
               <span className="text-lightLavender">/ 100</span>
             </div>
           </div>
-          <div className="flex justify-between w-[100%] bg-[hsla(39,_100%,_56%,_0.1)]">
-            <div className="flex">
-              <img src={Memory} alt="Memory" />
-              <span className="text-orangeYellow font-bold">Memory</span>
+          <div className="flex justify-between w-[100%] bg-[hsla(39,_100%,_56%,_0.1)] p-[0.5rem] items-center rounded-lg">
+            <div className="flex gap-[0.3rem]">
+              <img src={Memory} alt="Memory" className="w-[1.1rem]" />
+              <span className="text-orangeYellow font-bold text-[0.9rem]">
+                Memory
+              </span>
             </div>
-            <div>
-              <span className="text-grayBlue font-bold">80</span>
+            <div className="text-[0.9rem]">
+              <span className="text-grayBlue font-bold">92</span>
               <span className="text-lightLavender">/ 100</span>
             </div>
           </div>
-          <div className="flex justify-between w-[100%] bg-[hsla(166,_100%,_37%,_0.1)]">
-            <div className="flex">
-              <img src={Verbal} alt="Verbal" />
-              <span className="text-greenTeal font-bold">Verbal</span>
+          <div className="flex justify-between w-[100%] bg-[hsla(166,_100%,_37%,_0.1)] p-[0.5rem] items-center rounded-lg">
+            <div className="flex gap-[0.3rem]">
+              <img src={Verbal} alt="Verbal" className="w-[1.1rem]" />
+              <span className="text-greenTeal font-bold text-[0.9rem]">
+                Verbal
+              </span>
             </div>
-            <div>
-              <span className="text-grayBlue font-bold">80</span>
+            <div className="text-[0.9rem]">
+              <span className="text-grayBlue font-bold">61</span>
               <span className="text-lightLavender">/ 100</span>
             </div>
           </div>
-          <div className="flex justify-between w-[100%] bg-[hsla(234,_85%,_45%,_0.1)]">
-            <div className="flex">
-              <img src={Visual} alt="Visual" />
-              <span className="text-cobaltBlue font-bold">Visual</span>
+          <div className="flex justify-between w-[100%] bg-[hsla(234,_85%,_45%,_0.1)] p-[0.5rem] items-center rounded-lg">
+            <div className="flex gap-[0.3rem]">
+              <img src={Visual} alt="Visual" className="w-[1.1rem]" />
+              <span className="text-cobaltBlue font-bold text-[0.9rem]">
+                Visual
+              </span>
             </div>
-            <div>
-              <span className="text-grayBlue font-bold">80</span>
+            <div className="text-[0.9rem]">
+              <span className="text-grayBlue font-bold">72</span>
               <span className="text-lightLavender">/ 100</span>
             </div>
           </div>
-          <button>Continue</button>
+          <button className="bg-grayBlue text-primaryWhite w-[100%] py-[0.5rem] text-[0.9rem] font-bold rounded-[100vw] mt-[1rem]">
+            Continue
+          </button>
         </div>
       </div>
     </>
